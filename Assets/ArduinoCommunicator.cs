98bb40f0085312a8aba37b5f4a0ca9b66fef5adf
Johnny Rahajarison <rahajarison.johnny@gmail.com>
2015-05-23 12:34:35 +0200

    Commit initial Unity
    Ajout des fichiers Unity du drone Sim

---
+++ Assets/ArduinoCommunicator.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+
+public class ArduinoCommunicator : MonoBehaviour {
+	// TODO assurer le singleton
+
+	public string	serialPort = "COM3";
+	public int		bauds = 9600;
+
+	SerialPort stream = null;
+	// Use this for initialization
+	void	Start () {
+		connectToArduino ();
+	}
+	
+	// Update is called once per frame
+	void	Update () {
+	
+	}
+
+	void	createConnection()
+	{
+		stream = new SerialPort(serialPort, bauds);
+		stream.Open();
+//		stream.DataReceived += dataReceiver;
+//		stream.ReadTimeout = 200; // Assigne le timeout de read à 16 pour environ 60 FPS (trame non événementielle)
+		Debug.Log("Connection série effectuée !");
+	}
+
+//	void	dataReceiver(object sender, SerialDataReceivedEventArgs e)
+//	{
+//		SerialPort srlPort = (SerialPort) sender;
+//		byte[] buf = new byte[srlPort.BytesToRead];
+//		Debug.Log ("Data to read !");
+//		srlPort.Read (buf, 0, buf.Length);
+//		foreach (byte b in buf)
+//		{
+//			Debug.Log(b.ToString() + " ");
+//		}
+//	}
+
+	void	connectToArduino()
+	{
+		if (stream != null)
+		{
+			if (stream.IsOpen)
+			{
+				stream.Close();
+				Debug.LogWarning("Fermeture du port (déjà ouvert)");
+			}
+			else
+			{
+				createConnection ();
+			}
+		}
+		else
+		{
+			createConnection ();
+		}
+	}
+
+	public void sendByte(byte toSend)
+	{
+		byte[] dataToSend = new byte[1];
+		sendBytes (dataToSend);
+	}
+
+	public void	sendBytes(byte[] bytes)
+	{
+		Debug.Log ("Sending byte !");
+		stream.Write (bytes, 0, 1);
+	}
+
+	public int readByte()
+	{
+		int toReturn = stream.ReadByte ();
+		return toReturn;
+	}
+
+	void	OnApplicationQuit()
+	{
+		stream.Close ();
+	}
+}
---
+++ Assets/DroneController.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DroneController : MonoBehaviour {
+	public Text	throttleText;
+	public Text	yawText;
+	public Text	pitchText;
+	public Text	rollText;
+
+	public ArduinoCommunicator	arduino;
+	public delegate void pong();
+
+	float throttle = 0f;
+	float yaw = 0f;
+
+	bool	sended = false;
+
+	float	maxInputValue = 0.016f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		throttle = Input.GetAxis("Throttle") * Time.deltaTime;
+		yaw = Input.GetAxisRaw ("Yaw") * Time.deltaTime;
+
+	
+
+		throttleText.text = "Gaz : " + throttle;
+		yawText.text = "Lacet : " + yaw;
+		pitchText.text = "Angle d attaque : " + Input.GetAxisRaw("Pitch") * Time.deltaTime;
+		rollText.text = "Roulis : " + Input.GetAxisRaw("Roll") * Time.deltaTime;
+	
+		pingArduino ();
+	}
+
+
+	byte calculatePercentage(float value, float min, float max)
+	{
+		return (byte) Mathf.RoundToInt(((value + max) * 100) / (max - min));
+	}
+
+	void			sendCommand(byte toSend)
+	{
+		arduino.sendByte(toSend);
+		sended = true;
+		Invoke ("pongArduino", 0.5f);
+	}
+
+	void		  pingArduino()
+	{
+		if (!sended) {
+			byte convertedThrottle;
+
+			convertedThrottle = calculatePercentage (throttle, -maxInputValue, maxInputValue);
+			if (convertedThrottle > 80)
+			{
+				sendCommand(convertedThrottle);
+			}
+//			else if (yaw > 0.013f)
+//			{
+//				sendCommand((byte)'Y');
+//			}
+
+		}
+	}
+
+	void pongArduino()
+	{
+		byte response = (byte) arduino.readByte();
+		Debug.Log (response);
+		sended = false;
+	}
+}
